0794e3448e39150cea0d5b1bde6ebebd1adf9bf4
Kasumi <kasumi.null@yandex.com>
2023-01-19 15:34:30 -0600

    lint

--- scripts/001-deploy-soulbound-base.ts
+++ scripts/001-deploy-soulbound-base.ts
@@ -1,8 +1,12 @@
 const { ethers, upgrades } = require("hardhat");
 
 async function main() {
-  const SoulBoundBaseToken = await ethers.getContractFactory("SoulBoundBaseToken");
-  const soulboundBaseToken = await upgrades.deployProxy(SoulBoundBaseToken, [/* no construct params */]);
+  const SoulBoundBaseToken = await ethers.getContractFactory(
+    "SoulBoundBaseToken"
+  );
+  const soulboundBaseToken = await upgrades.deployProxy(SoulBoundBaseToken, [
+    /* no construct params */
+  ]);
   await soulboundBaseToken.deployed();
   console.log("SoulBoundBaseToken deployed to:", soulboundBaseToken.address);
 }
