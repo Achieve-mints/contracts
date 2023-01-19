9c2e858f61f9dbefc5f9280c721dfbf9e6697a47
Kasumi <kasumi.null@yandex.com>
2023-01-19 15:34:12 -0600

    add uristorage

--- contracts/SoulBoundBaseToken.sol
+++ contracts/SoulBoundBaseToken.sol
@@ -3,11 +3,12 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract SoulBoundBaseToken is ERC721EnumerableUpgradeable, AccessControlUpgradeable {
+contract SoulBoundBaseToken is ERC721EnumerableUpgradeable, ERC721URIStorageUpgradeable, AccessControlUpgradeable {
     using CountersUpgradeable for CountersUpgradeable.Counter;
 
     // stores the next tokenid
@@ -21,26 +22,41 @@ contract SoulBoundBaseToken is ERC721EnumerableUpgradeable, AccessControlUpgrade
         __ERC721_init("Test", "TST");
     }
 
+    // the following functions are overrides for compilation
+    function _burn(uint256 tokenId) internal virtual override(ERC721Upgradeable, ERC721URIStorageUpgradeable) {
+        return super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC721Upgradeable, ERC721EnumerableUpgradeable, AccessControlUpgradeable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+
     // since these are soulbound, we prevent any movement of tokens
     // TODO should we allow burning?
     function _beforeTokenTransfer(
         address from,
         address to,
-        uint256 /*tokenId*/,
+        uint256, /*tokenId*/
         uint256 /*batchSize*/
-    ) internal pure override(ERC721EnumerableUpgradeable) {
+    ) internal pure override(ERC721Upgradeable, ERC721EnumerableUpgradeable) {
         require(from == address(0) || to == address(0), "SoulBound cannot be transferred");
     }
 
-    // both ERC721EnumerableUpgradeable and AccessControlUpgradeable implement this
-    // so we must override and pass along to them
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        virtual
-        override(ERC721EnumerableUpgradeable, AccessControlUpgradeable)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
 }
